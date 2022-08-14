77aca0dfc6e492ec8812ca821d47a5c4a6492265
annyberin <annyberin@gmail.com>
2022-08-14 23:57:47 +0300

    Added current time & day

--- src/script.js
+++ src/script.js
@@ -1,3 +1,26 @@
+function currentDate(handleDate) {
+  let date = new Date(handleDate);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showtemperature(response) {
   console.log(response);
   console.log(response.data.main.temp);
@@ -11,6 +34,8 @@ function showtemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windSpeedElement = document.querySelector("#windSpped");
   windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = currentDate(response.data.dt * 1000);
 }
 
 let apiKey = "3e7e8fd8cf76af676ed5110468b54fe1";
